277635772853eeff795cd41542ecb9114442c908
Ronaldkubasu <ronald.kubasu@student.moringaschool.com>
2022-08-18 19:18:48 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+const num1 = 2
+const num2 = 31
+const num3 = 4
+const num4 = 2
+const multiply = num1*num2
+const random = parseInt(Math.random()*10);
+const mod = (num3%num4)+4
+const max = Math.max(20)
+
